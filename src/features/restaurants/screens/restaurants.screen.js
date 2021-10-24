f0cffb34190e85e4fef74a05e8e89c84f96090f0
Karam Elgamal <Karamjammal6@gmail.com>
2021-10-24 19:27:12 +0300

    App UI Updates

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+
+import { Searchbar } from 'react-native-paper';
+import { RestaurantInfoCard } from '../component/restaurant-info-card-component';
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.brand.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
